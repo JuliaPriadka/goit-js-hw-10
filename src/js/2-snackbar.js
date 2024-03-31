ae91e5bbf65d48f3cd779b0dea0ce6b6acfb49d3
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2024-03-31 21:15:32 +0300

    Update Snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,46 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
+const form = document.querySelector('.form');
+const formInput = document.querySelector('input');
 
+form.addEventListener('submit', onFormSubmit);
+
+formInput.min = '1000';
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const delay = event.target.elements.delay.value;
+  const state = event.target.elements.state.value;
+
+  const settledPromice = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'rejected') {
+        reject(`❌ Rejected promise in ${delay}ms`);
+      } else {
+          resolve(`✅ Fulfilled promise in ${delay}ms`);
+      }
+    }, delay);
+  });
+    
+  settledPromice
+      .then(value => {
+          console.log(value);
+                iziToast.show({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        backgroundColor: '#59a10d',
+        messageColor: '#fff',
+        position: 'topRight',
+      });
+      })
+      .catch(error => {
+        console.log(error);
+      iziToast.show({
+        message: `❌ Rejected promise in ${delay}ms`,
+        backgroundColor: '#ef4040',
+        messageColor: '#fff',
+        position: 'topRight',
+      });
+    });
+    form.reset();
+}
